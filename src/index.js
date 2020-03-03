fc2da2d5c7585fc611807a460c96535c5ba14672
unknown <mvieirinhaa@gmail.com>
2020-03-03 01:39:26 -0300

    commit -m "alterando a ordem do codigo pra funcionamento"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const app = express();
-
-app.listen(3000, () => {
-    console.log('servidor iniciado')
+app.listen(3001, () => {
+    console.log('pedro é gay')
 });
