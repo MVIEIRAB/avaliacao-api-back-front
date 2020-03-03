f4f542751e67924abbc5b1f08fb2889950344141
unknown <mvieirinhaa@gmail.com>
2020-03-03 01:31:47 -0300

    commit -m iniciando servidor da aplicação

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-// o desenvolvimento sera iniciado e foi editado
-// novo comentario apenas para teste
+const express = require('express');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('servidor iniciado')
+});
