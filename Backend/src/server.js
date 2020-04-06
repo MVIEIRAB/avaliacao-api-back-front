8413b07ae4e16b5a86109e2b328d400ea1ef3c77
unknown <mvieirinhaa@gmail.com>
2020-04-06 01:04:09 -0300

    configurando para o uso do frontend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 require('../src/controllers/authController')(app)
 require('../src/controllers/prodController')(app)
 
+app.use(express.static(__dirname + '/SPA'))
+
 app.listen(3333, () => {
     console.log('Server has been iniciated.')
 })
